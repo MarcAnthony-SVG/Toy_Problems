7948239b2d15cc451c699741ef213fe5f4350e34
Marc Rodriguez <60396817+MarcAnthony-SVG@users.noreply.github.com>
2020-03-18 14:32:26 -0500

    Re Done

--- newcommonCharacters/commonCharacters.js
+++ newcommonCharacters/commonCharacters.js
@@ -11,5 +11,20 @@
  */
 
 var commonCharacters = function(string1, string2) {
-  // TODO: Your code here!
+
+  var counter = 1; // counter to put replace the key
+  var charactersObj = {}; // charactersObj in order to have a key and value;
+  for (var i = 0; i < string1.length; i++) { // loops through string 1
+      charactersObj.value = string1[i]; // Gives each value a letter in the string; 
+      charactersObj.key = counter++; // Assigns each key a number 
+      console.log(charactersObj);
+  }
+  for (var j = 0; j < string2.length;j++) { // loops through string 2
+    // if (string2[j] === charactersObj.value) // checks to see if value at j is already in the object value
+    charactersObj.value = string2[j]; //Gives each value a letter in the string; 
+    charactersObj.key = counter++;
+    console.log(charactersObj);
+} 
+  return charactersObj; 
 };
+console.log(commonCharacters('acexivou', 'aegihobu'));
