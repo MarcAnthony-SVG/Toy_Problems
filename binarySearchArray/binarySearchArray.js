bd84093210b5c67ca5f494dc82e1fa6d982501f3
Marc Rodriguez <60396817+MarcAnthony-SVG@users.noreply.github.com>
2020-04-06 09:51:05 -0500

    "Binary Search Complete"

--- binarySearchArray/binarySearchArray.js
+++ binarySearchArray/binarySearchArray.js
@@ -11,5 +11,17 @@
  */
 
 var binarySearch = function(array, target) {
-
-};
+var index = target; // Set target to a value;
+if ( index > array.length || index < 0) { // Set up a conditional
+    return null;
+} else {
+    for  ( let i = 0; i < array.length; i++){ // Iterate through array
+        if (index === array[i]) {
+        index = array[i]-1;
+        // console.log(index);
+    }
+}
+return index
+}
+}
+console.log(binarySearch([1, 2, 3, 4, 5],4 ));
--- evenOccurence/evenOccurence.js
+++ evenOccurence/evenOccurence.js
@@ -10,6 +10,64 @@
  * console.log(onlyEven); //  4
  */
 
+// var evenOccurrence = function(arr) {
+//   var sorted = arr.sort();
+//   for (let i = 0; i < arr.length;i++){
+//     if (arr[i] !== arr[1]) {
+//       arr.shift(); 
+//       } else if (arr[i] === arr[1]) {
+//        return arr[i];
+//       }
+//   }
+// };
+// console.log(evenOccurrence([1, 7, 2, 4, 5, 6, 7, 8, 9, 6, 4]));
+
+
+// var evenOccurrence = function(arr) {  
+// var counter = 0;
+// var object = {
+//   value: 0,
+//   key: "null"
+// };
+// for (var element of arr) {
+//   object.key = element;
+//     console.log(object);
+//   }
+// for (var element of object){
+//   if (object[element] )
+// }
+// };
+// console.log(evenOccurrence([1, 7, 2, 4, 5, 6, 7, 8, 9, 6, 4]));
+
+
+// var evenOccurrence = function(arr) {
+//   var sorted = arr.sort();
+//   for (let i = 0; i < arr.length;i++){
+//     if (arr[i] !== arr[1]) {
+//       arr.shift(); 
+//       } else if (arr[i] === arr[1]) {
+//        return arr[i];
+//       }
+//   }
+// };
+// console.log(evenOccurrence([1, 7, 2, 4, 5, 6, 7, 8, 9, 6, 4]));
+
 var evenOccurrence = function(arr) {
-  // Your work here!
+	//key : ocurrence object
+	var obj = {};
+	//iterate over elements in input array
+	arr.forEach(element => {
+		//if element not a obj key
+		//set as obj key equal to 1; else current value plus 1
+		if(obj[element] === undefined) ? obj[element] = 1 : obj[element] = obj[element] + 1;
+	});
+	
+	//iterate through elements of input arr
+	for (let i = 0; i < arr.length; i++) {
+		//if element occurs an even amount of times
+		if (obj[arr[i]] % 2 === 0) { 
+			return arr[i]; }
+	}
+	return null;
 };
+console.log(evenOccurrence([1, 7, 2, 4, 5, 6, 7, 8, 9, 6, 4]));
