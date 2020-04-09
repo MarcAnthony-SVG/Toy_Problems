5eb0da5f69cfe775a7306230435bce9fec6f9107
Marc Rodriguez <60396817+MarcAnthony-SVG@users.noreply.github.com>
2020-04-09 09:46:10 -0500

    "quickSort"

--- quickSort/quickSort.js
+++ quickSort/quickSort.js
@@ -9,5 +9,29 @@
  *    Recursively sorts the two halves.
  *    Combines the two arrays and the pivot into a sorted array.
  */
+//1)func takes a array.
+//2)picks a value to pivot.
+//3)divide the array into to parts greater than and less than based on the pivot 
+//4)iterate over both arrays to get a them sorted.
+//5)combined the two arrays and return.
 
-var quicksort = function(array) {};
+var quicksort = function(array) { //func takes a array.
+var pivot = Math.floor(array.length/2); // picks a value to pivot.
+var greaterArr = [];
+var lowerArr = [];
+for(let num in array){
+    if(num > pivot){
+        greaterArr.push(num);
+    } else if (num < pivot) {
+        lowerArr.push(num);
+    }
+}
+return lowerArr.concat(pivot).concat(greaterArr);
+};
+console.log(quicksort([2,3,8,1,6,9,7,5,4,10]))
+
+
+    // const copyArr = [...array];
+    // var pivot = copyArr.reduce(function(a, b){
+    //     return a + b;
+    // }, 0);
