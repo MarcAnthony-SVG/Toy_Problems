dddfab42bab2f3a08e057353839c887f0ba14aaf
Marc Rodriguez <60396817+MarcAnthony-SVG@users.noreply.github.com>
2020-04-28 09:58:17 -0500

    "militaryTime"

--- militaryTime/militaryTime.js
+++ militaryTime/militaryTime.js
@@ -1,6 +1,19 @@
 // Write a function that takes in a string of the time and returns the time in military format.
 //Ex: militaryTime('7:47pm') returns '19:47'
 
+// if pm 
 function militaryTime(timeStr) {
-  //write code here
-}
+  const startTime = Array.from(timeStr);
+  // const timeArray = [];
+  console.log("first",startTime);
+  // const hours = [];
+  // const minutes = [];
+  const timeOfDay = startTime.slice(startTime.length-2).join('');
+  console.log(timeOfDay); // if(timeOfDay === pm)
+
+
+  for(let i = 0; i < timeStr.length; i++){
+    if(timeStr[i] === Number) {
+      console.log("I",timeStr[i])
+  }}}
+console.log(militaryTime('7:47pm'));
