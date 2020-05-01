fc46190e22c832d8b8008263fb431bfb65b115e6
Marc Rodriguez <60396817+MarcAnthony-SVG@users.noreply.github.com>
2020-05-01 10:02:04 -0500

    commit

--- balancedBrackets/balancedBracets.js
+++ balancedBrackets/balancedBracets.js
@@ -11,32 +11,48 @@
 // str: "[{]}"                    |   false
 
 const isBalanced = str => {
-    // const mainStorage = [];
+    const mainStorage = [];
     var parenthesis = [];
     var curlyBrackets = [];
     var brackets = [];
     for (let i = 0; i < str.length; i++){
-        if (str[i] === "(" || str[i] === ")"){
-            parenthesis.push(str[i]); 
-            console.log("parenthesis_if",parenthesis);
-        } else if (str[i] === "{" ||str[i] === "}" ){
-            curlyBrackets.push(str[i]); 
-            console.log("curlyBrackets_if",curlyBrackets)
-        } else if (str[i] === "[" || str[i] === "]") {
-            brackets.push(str[i]); 
-            console.log("brackets_if",brackets)
-        }
-        };
-    var parenthesis = parenthesis.join("");
-    console.log("parenthesis",parenthesis);
-    var curlyBrackets = curlyBrackets.join("");
-    console.log("brackets",brackets);
-    var brackets = brackets.join("");
-    console.log("curlyBrackets",curlyBrackets);
-    
-
+        for (let j = i+1; j < str.length; j++) {
+        if (str[j] === "(" || str[j] === ")" || str[j] === "{" ||str[j] === "}" || str[j] === "[" || str[j] === "]"){
+            mainStorage.push(str[j]); 
+        } else if (str[0] === "(" || str[0] === ")" || str[0] === "{" ||str[0] === "}" || str[0] === "[" || str[0] === "]" && )
+    }
+    return mainStorage;
+    console.log("mainStorage",mainStorage);
+        console.log(mainStorage);
+    }
     // for (let q = 0; q < parenthesis.length; q++){
     //     if (parenthesis[q] === "(")
     }
     // return storage; // [ '[', '{', '(', ')', '}', ']' ]
 console.log(isBalanced("[{]}"));
+
+
+// const isBalanced = str => {
+//     // const mainStorage = [];
+//     var parenthesis = [];
+//     var curlyBrackets = [];
+//     var brackets = [];
+//     for (let i = 0; i < str.length; i++){
+//         if (str[i] === "(" || str[i] === ")"){
+//             parenthesis.push(str[i]); 
+//             console.log("parenthesis_if",parenthesis);
+//         } else if (str[i] === "{" ||str[i] === "}" ){
+//             curlyBrackets.push(str[i]); 
+//             console.log("curlyBrackets_if",curlyBrackets)
+//         } else if (str[i] === "[" || str[i] === "]") {
+//             brackets.push(str[i]); 
+//             console.log("brackets_if",brackets)
+//         }
+//         };
+//     var parenthesis = parenthesis.join("");
+//     console.log("parenthesis",parenthesis);
+//     var curlyBrackets = curlyBrackets.join("");
+//     console.log("brackets",brackets);
+//     var brackets = brackets.join("");
+//     console.log("curlyBrackets",curlyBrackets);
+// console.log(isBalanced("[{]}"));
--- maxConsecutiveOnes/maxConsecutiveOnes.js
+++ maxConsecutiveOnes/maxConsecutiveOnes.js
@@ -1,7 +1,20 @@
 // Write a function that takes in an array of numbers and returns the maximum number of consecutive 1s
 
 //maxConsecutiveOnes([1,1,2,0,3,1,1,1,1,5,6,3]) returns 4
-
+// input: array of numbers [1,1,2,0,3,1,1,1,1,5,6,3]
+// output: number 4
+// constraints: 
+// edge cases: 
 function maxConsecutiveOnes(array) {
-  //write solution code
+  const storageA = [];
+  const storageB = [];
+  for (let i = 0; i < array.length; i++){
+    if (array[i] === 1){
+      storageA.push(array[i]);
+      i++;
+      // console.log("loop",storageA);
+    }
+  }
+  console.log(storageA);
 }
+console.log(maxConsecutiveOnes([1,1,2,0,3,1,1,1,1,6,3]));
