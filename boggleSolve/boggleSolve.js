087e07e04a7b96c374924d6f682a7a8333fddf9b
Marc Rodriguez <60396817+MarcAnthony-SVG@users.noreply.github.com>
2020-05-12 09:48:19 -0500

    boggleSort

--- boggleSolve/boggleSolve.js
+++ boggleSolve/boggleSolve.js
@@ -3,4 +3,69 @@
 // * i.e. a=1, z=26 etc.
 // return the sum of the values of each letter in string
 
-const boggleSolve = str => {};
+const boggleSolve = (str) => {
+    targetArray =[...str]; // [ 'b', 'a', 't' ]
+    alphabetCounterOBJ = {
+        a:0,
+        b:0,
+        c:0,
+        d:0,
+        e:0,
+        f:0,
+        g:0,
+        h:0,
+        i:0,
+        j:0,
+        k:0,
+        l:0,
+        m:0,
+        n:0,
+        o:0,
+        p:0,
+        q:0,
+        r:0,
+        s:0,
+        t:0,
+        u:0,
+        v:0,
+        w:0,
+        x:0,
+        y:0,
+        z:0
+     };
+    alphabetOBJ = {
+       a:1,
+       b:2,
+       c:3,
+       d:4,
+       e:5,
+       f:6,
+       g:7,
+       h:8,
+       i:9,
+       j:10,
+       k:11,
+       l:12,
+       m:13,
+       n:14,
+       o:15,
+       p:16,
+       q:17,
+       r:18,
+       s:19,
+       t:20,
+       u:21,
+       v:22,
+       w:23,
+       x:24,
+       y:25,
+       z:26
+    };
+    for (let i = 0; i < targetArray.length; i++){
+        if (targetArray['i'] === alphabetOBJ.key && alphabetOBJ.key === alphabetCounterOBJ.key){
+            alphabetCounterOBJ.key.value = 1;
+        }
+    }
+    return alphabetCounterOBJ;
+};
+console.log(boggleSolve("bat"));
--- romanNumeralTranslator/romanNumeralTranslator.js
+++ romanNumeralTranslator/romanNumeralTranslator.js
@@ -46,37 +46,38 @@ var translateRomanNumeral = function (romanNumeral) {
   };
   if (typeof romanNumeral !== "string") {
     return null;
-  } else if (romanNumeral === "I") {
-    return 1;
-  } else if (romanNumeral === "V") {
-    return 5;
-  } else if (romanNumeral === "X") {
-    return 10;
-  } else if (romanNumeral === "L") {
-    return 50;
-  } else if (romanNumeral === "C") {
-    return 100;
-  } else if (romanNumeral === "D") {
-    return 500;
-  } else if (romanNumeral === "M") {
-    return 1000;
+  // } else if (romanNumeral === "I") {
+  //   return 1;
+  // } else if (romanNumeral === "V") {
+  //   return 5;
+  // } else if (romanNumeral === "X") {
+  //   return 10;
+  // } else if (romanNumeral === "L") {
+  //   return 50;
+  // } else if (romanNumeral === "C") {
+  //   return 100;
+  // } else if (romanNumeral === "D") {
+  //   return 500;
+  // } else if (romanNumeral === "M") {
+  //   return 1000;
   }
   for (let i = 0; i < romanNumeral.length; i++) {
-    if (romanNumeral[i] === "I") {
-      storageOBJ.I += 1;
-    } else if (romanNumeral[i] === "V") {
-      storageOBJ.V += 5;
-    } else if (romanNumeral[i] === "X") {
-      storageOBJ.X += 10;
-    } else if (romanNumeral[i] === "L") {
-      storageOBJ.L += 50;
-    } else if (romanNumeral[i] === "C") {
-      storageOBJ.C += 100;
-    } else if (romanNumeral[i] === "D") {
-      storageOBJ.D += 500;
-    } else if (romanNumeral[i] === "M") {
-      storageOBJ.M += 1000;
-    }
+    // for (let j = 1; j < romanNumeral.length; j++){
+      if (romanNumeral[i] === "I") {
+        storageOBJ.I += 1;
+      } else if (romanNumeral[i] === "V") {
+        storageOBJ.V += 5;
+      } else if (romanNumeral[i] === "X") {
+        storageOBJ.X += 10;
+      } else if (romanNumeral[i] === "L") {
+        storageOBJ.L += 50;
+      } else if (romanNumeral[i] === "C") {
+        storageOBJ.C += 100;
+      } else if (romanNumeral[i] === "D") {
+        storageOBJ.D += 500;
+      } else if (romanNumeral[i] === "M") {
+        storageOBJ.M += 1000;
+      }
 
     // if (storageOBJ.I > 4) {
     //   storageOBJ.V - storageOBJ.I;
@@ -96,4 +97,4 @@ var translateRomanNumeral = function (romanNumeral) {
     storageOBJ.M
   );
 };
-console.log(translateRomanNumeral("MMXVI"));
+console.log(translateRomanNumeral("I"));
